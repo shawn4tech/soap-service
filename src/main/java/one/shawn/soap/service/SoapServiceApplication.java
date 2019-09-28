a8a3c49bf1b7ffb5958392530a8af5b27f2ffb01
shawn4tech <shawn.cui.tech@gmail.com>
2019-09-27 22:39:04 -0400

    Initialized project

---
+++ src/main/java/one/shawn/soap/service/SoapServiceApplication.java
@@ -0,0 +1,13 @@
+package one.shawn.soap.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/shawn/soap/service/SoapServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package one.shawn.soap.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
